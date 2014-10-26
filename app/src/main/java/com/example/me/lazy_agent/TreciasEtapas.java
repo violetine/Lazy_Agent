1eb0694b59af60615f18654fe2a725165f68c7a8
Paladin <aronax225@gmail.com>
2014-10-26 16:14:06 +0200

    prezentacijos versija appCamp

--- app/src/main/java/com/example/me/lazy_agent/TreciasEtapas.java
+++ app/src/main/java/com/example/me/lazy_agent/TreciasEtapas.java
@@ -26,94 +26,94 @@ import com.viewpagerindicator.CirclePageIndicator;
 /**
  * Created by Paladin on 10/25/2014.
  */
-public class TreciasEtapas extends FragmentActivity {
+public class TreciasEtapas extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.trecias_etapas_baze);
+        setContentView(R.layout.trecias_etapas_pirmas);
 
 
-        StatePageAdapter statePageAdapter = new StatePageAdapter(AntrasEtapasMenu.getSupportManager());
-        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        viewPager.setAdapter(statePageAdapter);
-
-        CirclePageIndicator circlePageIndicator = (CirclePageIndicator) findViewById(R.id.circle_page_ndicator);
-        circlePageIndicator.setViewPager(viewPager);
+//        StatePageAdapter statePageAdapter = new StatePageAdapter(AntrasEtapasMenu.getSupportManager());
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+//        viewPager.setAdapter(statePageAdapter);
+//
+//        CirclePageIndicator circlePageIndicator = (CirclePageIndicator) findViewById(R.id.circle_page_ndicator);
+//        circlePageIndicator.setViewPager(viewPager);
     }
 
-    class StatePageAdapter extends FragmentStatePagerAdapter
-    {
-        StatePageAdapter(FragmentManager fragmentManager)
-        {
-            super(fragmentManager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position)
-            {
-                case 0 :
-                    return new Pirmas();
-                case 1 :
-                    return new Antras();
-                case 2 :
-                    return new Trecias();
-            }
-            Fragment pirmas = new Pirmas();
-
-            return pirmas;
-        }
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
-
-        Drawable myDrawable = getResources().getDrawable(R.drawable.galaxy_s5);
-        @Override
-        public CharSequence getPageTitle(int position) {
-
-            SpannableStringBuilder sb = new SpannableStringBuilder(" "); // space added before text for convenience
-
-            myDrawable.setBounds(0, 0, myDrawable.getIntrinsicWidth(), myDrawable.getIntrinsicHeight());
-            ImageSpan span = new ImageSpan(myDrawable, ImageSpan.ALIGN_BASELINE);
-            sb.setSpan(span, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-            return "s";
-        }
-    }
-
-    public static class Pirmas extends Fragment
-    {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.trecias_etapas_pirmas,container,false); // per root view galima pagaut tevo elementus
-
-            return rootView;
-        }
-    }
-    public static class Antras extends Fragment
-    {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.trecias_etapas_antras,container,false); // per root view galima pagaut tevo elementus
-
-            return rootView;
-        }
-    }
-
-    public static class Trecias extends Fragment
-    {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.trecias_etapas_trecias,container,false); // per root view galima pagaut tevo elementus
-
-            return rootView;
-        }
-    }
+//    class StatePageAdapter extends FragmentStatePagerAdapter
+//    {
+//        StatePageAdapter(FragmentManager fragmentManager)
+//        {
+//            super(fragmentManager);
+//        }
+//
+//        @Override
+//        public Fragment getItem(int position) {
+//            switch (position)
+//            {
+//                case 0 :
+//                    return new Pirmas();
+//                case 1 :
+//                    return new Antras();
+//                case 2 :
+//                    return new Trecias();
+//            }
+//            Fragment pirmas = new Pirmas();
+//
+//            return pirmas;
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return 3;
+//        }
+//
+//        Drawable myDrawable = getResources().getDrawable(R.drawable.galaxy_s5);
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//
+//            SpannableStringBuilder sb = new SpannableStringBuilder(" "); // space added before text for convenience
+//
+//            myDrawable.setBounds(0, 0, myDrawable.getIntrinsicWidth(), myDrawable.getIntrinsicHeight());
+//            ImageSpan span = new ImageSpan(myDrawable, ImageSpan.ALIGN_BASELINE);
+//            sb.setSpan(span, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//
+//            return "s";
+//        }
+//    }
+//
+//    public static class Pirmas extends Fragment
+//    {
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//
+//            View rootView = inflater.inflate(R.layout.trecias_etapas_pirmas,container,false); // per root view galima pagaut tevo elementus
+//
+//            return rootView;
+//        }
+//    }
+//    public static class Antras extends Fragment
+//    {
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//
+//            View rootView = inflater.inflate(R.layout.trecias_etapas_antras,container,false); // per root view galima pagaut tevo elementus
+//
+//            return rootView;
+//        }
+//    }
+//
+//    public static class Trecias extends Fragment
+//    {
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//
+//            View rootView = inflater.inflate(R.layout.trecias_etapas_trecias,container,false); // per root view galima pagaut tevo elementus
+//
+//            return rootView;
+//        }
+//    }
 
 }
